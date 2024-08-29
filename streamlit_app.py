fde1504329b10ee29e7492aff716d5b3aa27be7a
rishi006999 <157061716+rishi006999@users.noreply.github.com>
2024-08-29 15:53:49 +0000

    replace

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
 from openai import OpenAI
 
+
+# Show title and description.
+st.title("📄 Document question answering agent")
+st.write(
+    "Upload a document below and ask a question about it – GPT will answer! "
+    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+)
+
+# Ask user for their OpenAI API key via `st.text_input`.
+# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
+# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
+openai_api_key = st.text_input("OpenAI API Key", type="password")
+
 def validate_api_key():
     if openai_api_key:
         try:
@@ -14,18 +27,6 @@ def validate_api_key():
             st.session_state["api_key_valid"] = False
     else:
         st.error("Please enter your OpenAI API key.")
-
-# Show title and description.
-st.title("📄 Document question answering agent")
-st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-)
-
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
 
 
 if not openai_api_key:
